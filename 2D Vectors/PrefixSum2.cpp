af65235bbeb399f964551182ebc14f89b597fb6b
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-05-06 11:53:26 +0530

    Adding files

---
+++ 2D Vectors/PrefixSum2.cpp
@@ -0,0 +1,2 @@
+//Given a matrix "a" of dimension nxm and 2 coordinates (l1,r1) and (l2,r2). Return the sum of the rectangle from (l1,r1) to (l2,r2)
+// Method : Pre-calculating the horizontal sum for each row in the matrix
