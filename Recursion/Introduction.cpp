c1bc135b671af8d8060a782df87a661635b6b196
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-06-17 10:55:35 +0530

    Recursion

---
+++ Recursion/Introduction.cpp
@@ -0,0 +1,2 @@
+// Definition: In recursion we try to solve a bigger problem by finding out solution to smaller sub-problems. We represent these problems in the form of functions and these function calls itself to solve a smaller sub-problems.
+
---
+++ Recursion/factorial_recursive.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int f(int n){
+    //base case
+    if(n==1){
+        return 1;
+    }
+    int ans = n*f(n-1);
+    return ans;
+
+    //or
+    //if(n==1) return 1;
+    //return n*f(n-1);
+
+}
+
+int main(){
+    int result = f(5);
+    cout<<result<<endl;
+    return 0;
+}
