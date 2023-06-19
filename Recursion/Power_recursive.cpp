4b8693783bd52767b3157e2118b5aa07a13d4398
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-06-19 23:27:28 +0530

    update

--- Recursion/Power_recursive.cpp
+++ Recursion/Power_recursive.cpp
@@ -3,16 +3,23 @@
 #include <iostream>
 using namespace std;
 
-//f(p,q) =  p*f(p,q-1)
+// f(p,q) =  p*f(p,q-1)
 
-int f(int p, int q){
-    //base case
-    if(q==0) return 1;
-    return p*f(p,q-1);
+int f(int p, int q)
+{
+    // base case
+    if (q == 0)
+        return 1;
+    return p * f(p, q - 1);
 }
 
-int main(){
-    int result = f(5,2);
-    cout<<result<<endl;
+int main()
+{
+    int result = f(5, 2);
+    cout << result << endl;
     return 0;
 }
+
+
+//Time complexity = 0(q)
+//space complexity = 0(q)
