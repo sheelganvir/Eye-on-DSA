1d69ebcfb2760d18c59fec7b9b29b4bba10f47d5
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-06-21 17:34:41 +0530

    Recursive array files

---
+++ Recursion/Print_array_recursive.cpp
@@ -0,0 +1,21 @@
+//Given an array, print all the elements of the array recursively
+
+#include <iostream>
+using namespace std;
+
+void f(int *arr, int idx, int n){
+    //base case
+    if(idx==n) return;
+
+    //self work
+    cout<<arr[idx]<<endl;
+
+    //assume that
+    f(arr, idx+1, n); //assume it works correctly -> it will print the remaining array
+}
+int main(){
+    int n=5;
+    int arr[]={6,1,9,3,4};
+    f(arr,0,n);
+    return 0;
+}
