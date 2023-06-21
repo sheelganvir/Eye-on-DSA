ce71333d0b9d2a6af7bb60b6b9293158e00ef8e4
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-06-21 21:28:23 +0530

    Create Sum_array_recursive.cpp

---
+++ Recursion/Sum_array_recursive.cpp
@@ -0,0 +1,23 @@
+// Find the sum of values of the array [2,3,5,20,1]
+#include <iostream>
+using namespace std;
+
+int f(int *arr, int idx, int n)
+{
+    // base case
+    int sum = 0;
+    if (idx == n - 1)
+    {
+        // idx is at the last index so there is only one element under consideration
+        return arr[idx];
+    }
+    return arr[idx] + f(arr, idx + 1, n);
+}
+
+int main()
+{
+    int n = 5;
+    int arr[] = {2, 3, 5, 20, 1};
+    cout << f(arr, 0, n);
+    return 0;
+}
