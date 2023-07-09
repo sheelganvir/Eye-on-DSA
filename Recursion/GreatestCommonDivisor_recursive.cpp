eeeeb8b360fcc2996d186a57e2cc685a9dfd14d7
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-09 21:37:49 +0530

    New recursive question

---
+++ Recursion/GreatestCommonDivisor_recursive.cpp
@@ -0,0 +1,21 @@
+/*Given twi numbers x and y. Find the greatest common divisor or HCF of x and y using recursive
+
+Constraints : x>=0 , y<=10^6
+Input: x=4 y=9
+Output: 1
+
+gcd(a,b) = gcd(b, a%b) ; a>b
+*/
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b>a) return gcd(b,a);
+    if(b==0) return a;  //base case
+    return gcd(b,a%b);
+}
+int main(){
+    cout<<gcd(48,8);
+    return 0;
+}
