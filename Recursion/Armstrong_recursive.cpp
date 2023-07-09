690b33b63f751a9d8cc402c1847beece8f469c4c
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-09 22:19:37 +0530

    Create Armstrong_recursive.cpp

---
+++ Recursion/Armstrong_recursive.cpp
@@ -0,0 +1,16 @@
+/* Given a number n. Print if it is a armstrong or not.
+
+An armstrong number is a number if the sum of every digits int that number raised to the power of total digits in that number is equal to that number.
+
+Input: 153
+Output: Yes
+
+153 = 1^3 + 5^3 + 3^3 = 1+125+27 = 153 hence 153 is a armstrong number.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
