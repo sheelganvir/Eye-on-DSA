4e1750253af55cff13a53430b372bf09fc3783e2
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-10 17:50:45 +0530

    Update Armstrong_recursive.cpp

--- Recursion/Armstrong_recursive.cpp
+++ Recursion/Armstrong_recursive.cpp
@@ -6,6 +6,9 @@ Input: 153
 Output: Yes
 
 153 = 1^3 + 5^3 + 3^3 = 1+125+27 = 153 hence 153 is a armstrong number.
+
+f(n,d) = pow(n%10,d) + f(n/10,d)
+
 */
 #include <iostream>
 using namespace std;
