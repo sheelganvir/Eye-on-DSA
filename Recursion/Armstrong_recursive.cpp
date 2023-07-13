7eb5ebb392673c70b09d78fb39314875e54312c8
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-13 06:49:12 +0530

    Update armstrong recursive

--- Recursion/Armstrong_recursive.cpp
+++ Recursion/Armstrong_recursive.cpp
@@ -13,7 +13,49 @@ f(n,d) = pow(n%10,d) + f(n/10,d)
 #include <iostream>
 using namespace std;
 
+int pow_recursive(int p, int q)
+{
+    // base case
+    if (q == 0)
+        return 1;
+    if (q % 2 == 0)
+    {
+        // If q is even
+        int result = pow_recursive(p, q / 2);
+        return result * result;
+    }
+    else
+    {
+        // If q is odd
+        int result = pow_recursive(p, (q - 1) / 2);
+        return p * result * result;
+    }
+} 
+
+int f(int n, int d){
+    //base case
+    if(n==0) return 0;
+    return pow_recursive(n%10,d) + f(n/10, d);
+}
+
 int main(){
-    parents are gem , 
+    int n;
+    cin>>n;
+
+    int no_of_digits=0;
+    int temp = n;
+
+    while(temp>0){
+        temp=temp/10;
+        no_of_digits++;
+    } 
+
+    int result = f(n,no_of_digits);
+    if(result==n){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
     return 0;
 }
