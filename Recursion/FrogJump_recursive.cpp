14e8a82a3bb168c331e83a1663702cf854cbdeb6
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-14 07:35:49 +0530

    Create FrogJump_recursive.cpp

---
+++ Recursion/FrogJump_recursive.cpp
@@ -0,0 +1,16 @@
+/*There are N stones, numbered 1,2,,..,N. For each i (1si<N), the height of Stone i is hi.There
+ is a frog who is initially on Stone 1. Не will repeat the following action some number of
+times to reach Stone N:
+
+ If the frog is currently on Stone i, jump to Stone i+1 or Stone i+2. Here, a cost of |hi- hj|
+ is incurred, where j is the stone to land on.
+
+ Find the minimum possible total cost incurred before the frog reaches Stone N.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
