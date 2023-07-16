1a919c27c5f0877cda2f57713e0eb4e32ba6b7ea
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-16 10:58:25 +0530

    Adding recursive file

--- Recursion/Existence_of_num_in_arrayRecursive.cpp
+++ Recursion/Existence_of_num_in_arrayRecursive.cpp
@@ -5,12 +5,27 @@ Inputl : n = 8,x = 14, array = [4,12, ,54,14,3,8,6,1]
 Output1 : Yes
 Input2:n =1 , x= 9, array = [2]
 Output2 : No
+
+f(arr,n,i,x) = (arr[i]==x) || f(arr,n,i+1,x);
 */
 
 #include <iostream>
 using namespace std;
 
+bool f(int *arr, int n, int i, int x){
+    //base case
+    if(i==n){
+        //array is exhausted
+        return false;
+    }
+    return (arr[i]==x) || f(arr,n, i+1, x);
+}
 int main(){
-    
+    int n = 9;      //size
+    int arr[] = {4,12,9,54,14,3,8,6,1};
+    int x = 14; 
+    bool result = f(arr,n,0,x);
+    if(result) cout<<"Yes";
+    else cout<<"No";
     return 0;
 }
