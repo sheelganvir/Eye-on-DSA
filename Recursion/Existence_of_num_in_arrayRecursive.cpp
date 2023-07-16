fa075989c65b216e113e5d20eddd21bbe113c267
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-16 10:32:11 +0530

    Create Existence_of_num_in_arrayRecursive.cpp

---
+++ Recursion/Existence_of_num_in_arrayRecursive.cpp
@@ -0,0 +1,16 @@
+/*Given an array of n integers and a target value x.Print whether x exists in the
+array or not.
+Constraints:0<=n <=le6, -le8 <= x < = le8, -le8 <= a[i] <= le8
+Inputl : n = 8,x = 14, array = [4,12, ,54,14,3,8,6,1]
+Output1 : Yes
+Input2:n =1 , x= 9, array = [2]
+Output2 : No
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
