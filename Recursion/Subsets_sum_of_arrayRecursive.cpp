7dac8f7449b58993d49662a2a8b2e954bb6bcb7f
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-20 07:47:00 +0530

    Adding file recursive sum of subsets

--- Recursion/Subsets_sum_of_arrayRecursive.cpp
+++ Recursion/Subsets_sum_of_arrayRecursive.cpp
@@ -7,13 +7,29 @@ Output:0 2 4 5 6 7 9 11
 Hint: Total subsets for a set of length n = 2^n
 
 f(arr,n,idx,sum,result) = f(arr,n,idx+1,sum+arr[idx],result)
-                          f(arr,n.idx+1,sum,result)
+                          f(arr,n,idx+1,sum,result)
 */
 
 #include <iostream>
+#include<vector>
 using namespace std;
 
+void f(int *arr, int n, int i, int sum, vector<int> &result){
+    //base case
+    if(i==n){
+        result.push_back(sum);
+        return;
+    }
+    f(arr,n,i+1,sum+arr[i],result);
+    f(arr,n,i+1,sum,result);
+}
 int main(){
-    I LOVE u 
+    int arr[]={2,4,5};
+    int n=3;
+    vector<int>result;
+    f(arr,n,0,0,result);
+    for(int i=0; i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
     return 0;
 }
