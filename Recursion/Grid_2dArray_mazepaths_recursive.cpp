92d2a4c3d6024f06f172e9e7ea2f48db1808aa83
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-07-23 07:34:12 +0530

    update Grid_2dArray_mazepaths_recursive

--- Recursion/Grid_2dArray_mazepaths_recursive.cpp
+++ Recursion/Grid_2dArray_mazepaths_recursive.cpp
@@ -11,7 +11,13 @@ top left (grid[o][o]) to bottom right (grid[m-1][n-1]).
 #include <iostream>
 using namespace std;
 
+int f(int i, int j, int m, int n) {
+    if(i== m-1 and j== n-1) return 1;
+    if(i>=m or j>=n) return 0;
+    return f(i, j + 1, m, n) + f(i+1, j, m, n);
+}
+
 int main(){
-    
+    cout<<f(0,0,3,3);
     return 0;
 }
