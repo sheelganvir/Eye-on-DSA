fd85715f6434d1925db95d6167cb85576469aa50
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-10-15 10:01:39 +0530

    Adding Selection Sort [TS: O(n^2) and SC: O(1)]

---
+++ Sorting/Selection_sort.cpp
@@ -0,0 +1,62 @@
+/*SELECTION SORT ALGORITHM : Repeatedly find minimum element in unsorted array and place it at the beginning.
+
+In ith iteration we are selecting the element for ith element
+
+Time Complexity : O(n^2)
+Space Complexity : O(1)
+
+Selection sort is unsorted because order of elements with the same value are disturbed
+
+Note: We prefer selection sort over bubble sort for its worst case time complexity*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &v)
+{
+
+    int n = v.size();
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+
+        // finding min element in unsorted array
+        int minIndex = i;
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (v[j] < v[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        // placing minimum element at the beginning
+        if (i != minIndex)
+        {
+            swap(v[i], v[minIndex]);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    selectionSort(v);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
