a08c92df5b3d21642c302cdb16327f995c31469c
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-10-15 10:02:00 +0530

    Update Bubble_sort.cpp

--- Sorting/Bubble_sort.cpp
+++ Sorting/Bubble_sort.cpp
@@ -1,17 +1,25 @@
-/* BUBBLE SORT ALGORITHM : repeatedly swap two adjacent elemsnts if in wrong orders*/
+/* BUBBLE SORT ALGORITHM : repeatedly swap two adjacent elemsnts if in wrong orders
+
+Time Complexity : O(n^2)
+Space Complexity : O(1)
+
+Bubble sort is sorted because order of elements with the same value are not disturbed*/
 
 #include <iostream>
 #include<vector>
 using namespace std;
 
 void bubbleSort(vector<int> &v){
+    bool flag = false;
     int n = v.size();
     for (int i=0 ;i<n-1; i++){
         for(int j=0 ; j< n-i-1 ; j++){
             if(v[j]>v[j+1]){
+                flag= true;
                 swap(v[j],v[j+1]);
             }
         }
+        if(!flag) break;
     }
     return;
 }
