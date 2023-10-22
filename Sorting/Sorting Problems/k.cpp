64808e25968fd133711f67495a6decb0493b2e29
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-10-22 11:12:16 +0530

    updating file

--- Sorting/Sorting Problems/k.cpp
+++ Sorting/Sorting Problems/k.cpp
@@ -1,41 +1,43 @@
-
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int removeDuplicates(vector<int> &a, int n)
-{
-    int cnt = 0;
-    remove(a.begin(),a.end(),a[n]);
-    for (int i = 1; i < n; i++)
-    {
-        if (nums[i] == nums[i - 1])
-        {
-            nums.erase(i);
+bool isEtherealWrap(vector<int> code) {
+    int n = code.size();
+    
+    // Sort the code array
+    sort(code.begin(), code.end());
+    
+    for (int i = 1; i < n; i++) {
+        if (code[i] - code[i - 1] > 1) {
+            return false;
         }
     }
-    return nums.size();
+    
+    return true;
 }
 
-int main()
-{
-    int n;
-    cin >> n;
-
-    vector<int> v(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i];
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> code(n);
+        
+        for (int i = 0; i < n; i++) {
+            cin >> code[i];
+        }
+        
+        if (isEtherealWrap(code)) {
+            cout << "Yes, it is Ethereal Wrap" << endl;
+        } else {
+            cout << "No, it is a normal invisibility cloak" << endl;
+        }
     }
-
-    removeDuplicates(v);
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << v[i] << " ";
-    }
-    cout << endl;
+    
     return 0;
 }
