12ee44f862621998339f8c7b12326289edf47f4d
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-10-30 15:22:56 +0530

    update

--- Sorting/Sorting Problems/k.cpp
+++ Sorting/Sorting Problems/k.cpp
@@ -1,55 +1,38 @@
-/**
- * The code takes an input array of integers, rearranges the positive numbers to even indices and
- * negative numbers to odd indices, and returns the rearranged array.
- * 
- * param nums The parameter `nums` is a vector of integers.
- * 
- * return The `rearrangeArray` function returns a vector of integers.
- */
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n =nums1.size();
+        int m = nums2.size();
+        int k = n+m;
+        vector<int> temp(k);
+        
+        int a=0;
+        int b=0;
+        int c=0;
 
-// -1 2 -3 5 6 8 -10 15 -16 -9
-// 
-
-vector<int> rearrangeArray(vector<int>& nums) {
-        int n= nums.size();
-        int ai=0;
-        int bi=1;
-        vector<int> newman(nums.size());
-        /* The line `for(int i=0 ; i< n ; i++)` is a for loop that iterates over the elements of the
-        `nums` vector. It starts with `i` equal to 0 and continues as long as `i` is less than the
-        size of the `nums` vector (`n`). After each iteration, `i` is incremented by 1. */
-        for(int i=0 ; i< n ; i++){
-            if(nums[i]>=0){
-                /* The line `newman[ai]=nums[i];` is assigning the value of `nums[i]` to the element at
-                index `ai` in the `newman` vector. This is part of the process of rearranging the
-                positive numbers to even indices in the `newman` vector. */
-                newman[ai]=nums[i];
-                ai+=2;
+        while (a<n && b<m){
+            if (nums1[a] <= nums2[b]) {
+                temp[c++]=nums1[a++];
             }
-            else{
-                newman[bi]=nums[i];
-                bi+=2;
+            else {
+                temp[c++]=nums2[b++];
             }
         }
-        
-        return newman;
+
+        while(a<n1){
+            temp[c++]=nums1[a++];
+        }
+
+        while(b<n2){
+            temp[c++]=nums2[b++];
+        }
     }
-
-int main()
-{
-      vector<int> v(10);
-      for (int i = 0; i < 10; i++)
-      {
-            cin >> v[i];
-      }
-
-      rearrangeArray(v);
-      
-      for (int i = 0; i < 10; i++)
-      {
-            cout<< v[i]<<" ";
-      }
-      return 0;
-}
+    double ans=0;
+    if(k%2!=0){
+        ans=temp[k/2];
+    }
+    else{
+        ans=(temp[(k/2)-1]+temp[k/2])/2;
+    }
+    return ans;
+};
