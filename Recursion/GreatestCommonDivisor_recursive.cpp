12c52fb0017a2ab86066c192db44a96d4ab67e25
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-11-05 10:50:33 +0530

    updzte recursive files

--- Recursion/GreatestCommonDivisor_recursive.cpp
+++ Recursion/GreatestCommonDivisor_recursive.cpp
@@ -15,6 +15,7 @@ int gcd(int a, int b){
     if(b==0) return a;  //base case
     return gcd(b,a%b);
 }
+
 int main(){
     cout<<gcd(48,8);
     return 0;
--- Sorting/Sorting Problems/k.cpp
+++ Sorting/Sorting Problems/k.cpp
@@ -1,38 +1,24 @@
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n =nums1.size();
-        int m = nums2.size();
-        int k = n+m;
-        vector<int> temp(k);
-        
-        int a=0;
-        int b=0;
-        int c=0;
+#include <iostream>
+#include <vector>
+using namespace std;
+int findTournamentChampion(vector<vector<int>>& grid) {
+    int n = grid.size();
+    
+    for (int i = 0; i < n; i++) {
+        bool isChampion = true;
 
-        while (a<n && b<m){
-            if (nums1[a] <= nums2[b]) {
-                temp[c++]=nums1[a++];
-            }
-            else {
-                temp[c++]=nums2[b++];
+        for (int j = 0; j < n; j++) {
+            if (i != j && grid[i][j] != 1) {
+                isChampion = false;
+            
             }
         }
 
-        while(a<n1){
-            temp[c++]=nums1[a++];
+        if (isChampion) {
+            return i;
         }
+    }
 
-        while(b<n2){
-            temp[c++]=nums2[b++];
-        }
-    }
-    double ans=0;
-    if(k%2!=0){
-        ans=temp[k/2];
-    }
-    else{
-        ans=(temp[(k/2)-1]+temp[k/2])/2;
-    }
-    return ans;
-};
+    return -1;  
+}
+
