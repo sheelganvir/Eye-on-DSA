50e9334613e0782182a8c7475c84fe59fab4b3ca
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-11-29 22:09:15 +0530

    Update FindTarget_SortedRotatedArray.cpp

--- Binary Search/FindTarget_SortedRotatedArray.cpp
+++ Binary Search/FindTarget_SortedRotatedArray.cpp
@@ -8,9 +8,46 @@
 
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int binarySearchSortedRotated(vector<int>&input,int target){
+    int lo=0, hi=input.size()-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(input[mid]==target) return mid;
+        if(input[mid]>=input[lo]){
+            if(target>input[lo] and target<=input[mid]){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }else{
+            if(target>=input[mid] and target<=input[hi]){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+    }
+    return  -1;
+}
 
 int main(){
     
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i=0;i<n;i++){
+        cin>>input[i];
+    }
+    int target;
+    cin>>target;
+    
+    cout<<binarySearchSortedRotated(input,target)<<endl;
+
+    return 0;
     return 0;
 }
