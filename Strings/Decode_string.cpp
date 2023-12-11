573cd4121d07d4b35d3c9978a6d4b4a930121a02
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-11 20:11:38 +0530

    UPDATE DSA

--- Strings/Decode_string.cpp
+++ Strings/Decode_string.cpp
@@ -14,12 +14,59 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+string decodedString(string s){
+    string result = "";
 
+    //traversing the encoded string
+    for(int i=0 ; i<s.length(); i++){
+        if(s[i]!=']'){
+            result.push_back(s[i]);
+        }
+        else{
+            string str = "";
+            while(!result.empty() && result.back()!='['){
+                str.push_back(result.back());
+                result.pop_back();
+            }
+
+            //resversing the str
+            reverse(str.begin(), str.end());
+
+            //remove last char from result which is [
+            result.pop_back();
+
+            //extract num from result
+            string num = "";
+            while(!result.empty() && (result.back()>='0' && result.back()<='9')){
+                num.push_back(result.back());
+                result.pop_back();
+            }
+
+            //reversing the num string
+            reverse(num.begin(),num.end());
+
+            //convert string to integer (STOI method)
+            int int_num = stoi(num); 
+
+            //inserting str in result int_num times
+            while(int_num){
+                result+=str;
+                int_num--;
+            }
+        }
+    }
+    return result;
+}
 
 int main(){
     
+    string str;
+    cin>>str;
+
+    cout<<decodedString(str)<<endl;
     return 0;
 }
