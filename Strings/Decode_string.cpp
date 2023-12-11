3b85d97a22d28db25caba4997c35c44bb26eff96
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-11 10:23:49 +0530

    Create Decode_string.cpp

---
+++ Strings/Decode_string.cpp
@@ -0,0 +1,25 @@
+/*
+    An encoded string(s) is given, and the task is to decode it. The encoding pattern is that 
+    the occurence of the string is given at the starting of the string and each string is 
+    enclosed by square brackets
+    Note: The occurence of a sngle string is less than 1000
+
+    Input1: s = 1[b]
+    Output1: b
+
+    Input2: s = 3[b2[ca]]
+    Output: bcacabcacabcaca
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+    
+    return 0;
+}
