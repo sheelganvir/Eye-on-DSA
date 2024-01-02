ad499a2f55093274bc6295bbc98cdba049b29ccd
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-02 20:08:57 +0530

    Update IntersectionLL.cpp

--- Linked List/IntersectionLL.cpp
+++ Linked List/IntersectionLL.cpp
@@ -63,23 +63,41 @@ int getLength(Node* head){
     return length;
 }
 
+Node* moveHeadByK(Node* head, int k){
+    Node* ptr = head;
+    while(k--){
+        ptr=ptr->next;
+    }
+    return ptr;
+}
 
-bool CheckIntersectionLL(Node* head1, Node* head2){
+Node* getIntersection(Node* head1, Node* head2){
+    //step1: calculate lenghts of both thee linked list
+    int l1 = getLength(head1);
+    int l2 = getLength(head2);
 
-    Node* ptr1 = head1;
-    Node* ptr2 = head2;
-
-    while(ptr1!=NULL || ptr2!=NULL){
-        if(ptr1->val != ptr2->val){
-            return false;
+    //step2: find the differene k between linkedlist and move longer linkedlist ptr by k steps ahead
+    Node* ptr1;
+    Node* ptr2;
+    if(l1>l2){  //ll1 is longer
+        int k = l1-l2;
+        ptr1 = moveHeadByK(head1, k);
+        ptr2 = head2;
+    }else{  //ll2 is longer
+        int k =l2-l1;
+        ptr1 = head1;
+        ptr2 = moveHeadByK(head2, k );
+    }
+    while(ptr1){
+        if(ptr1==ptr2){
+            return ptr1;
         }
         ptr1=ptr1->next;
         ptr2=ptr2->next;
     }
-
-    //at this point either ptr1 is null, or ptr2 is null, or both are null
-    return (ptr1==NULL && ptr2==NULL);
+    return NULL;
 }
+
 
 int main(){
     
@@ -88,16 +106,27 @@ int main(){
     ll1.insertAtTail(2);
     ll1.insertAtTail(3);
     ll1.insertAtTail(4); 
+    ll1.insertAtTail(5); 
+    ll1.insertAtTail(6); 
+
 
     LinkedList ll2;
-    ll2.insertAtTail(1);
-    ll2.insertAtTail(2);
-    ll2.insertAtTail(3);
-    ll2.insertAtTail(4); 
+    ll2.insertAtTail(6);
+    ll2.insertAtTail(20);
     
-    ll1.display();
-    ll2.display();
+    ll2.head->next->next = ll1.head->next->next->next;
+ 
+    
+    ll1.display();  //1->2->3->4->5->6->NULL
+    ll2.display();  //6->20->4->5->6->NULL
 
-    cout<<CheckIntersectionLL(ll1.head, ll2.head);
+    Node* intersection = getIntersection(ll1.head, ll2.head);
+
+    if(intersection){
+        cout<<intersection->val<<endl;
+    }
+    else{
+        cout<<"-1"<<endl;
+    }
     return 0;
 }
