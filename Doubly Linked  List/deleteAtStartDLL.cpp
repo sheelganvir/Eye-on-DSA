df73760c2213c047f7160a14837c517d127dbe4e
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-19 22:30:13 +0530

    Create deleteAtStartDLL.cpp

---
+++ Doubly Linked  List/deleteAtStartDLL.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    
+    Node(int data){
+        val = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+class DoublyLinkedList{
+public:
+    Node* head;
+    Node* tail;
+
+    DoublyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void display(){
+        if (head == NULL) {
+            cout << "The list is empty." << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }cout<<endl;
+    }
+
+    void insertAtStart(int val){
+        Node* newNode = new Node(val);
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        return;
+    }
+};
+
+int main(){
+    
+    DoublyLinkedList dll;
+    dll.insertAtStart(1);
+    dll.display();
+    dll.insertAtStart(2);
+    dll.display();
+    dll.insertAtStart(3);
+    dll.display();
+
+    return 0;
+}
