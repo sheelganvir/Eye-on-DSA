f29bb97d329372a2b3072f2717e14bccf266a307
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-21 21:42:43 +0530

    Create palindromeDLL.cpp

---
+++ Doubly Linked  List/palindromeDLL.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    
+    Node(int data){
+        val = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+class DoublyLinkedList{
+public:
+    Node* head;
+    Node* tail;
+
+    DoublyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void display(){
+        if (head == NULL) {
+            cout << "The list is empty." << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }cout<<endl;
+    }
+
+    void insertAtEnd(int val){
+        Node* newNode = new Node(val);
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+        return;
+    }
+
+    bool checkPalindrome(Node* &head, Node* &tail){
+        if(head == NULL || head == tail)return true;
+
+        
+    }
+};
+
+int main(){
+    
+    DoublyLinkedList dll;
+    dll.insertAtEnd(1);
+    dll.insertAtEnd(2);
+    dll.insertAtEnd(3);
+    dll.insertAtEnd(4);
+    dll.display();
+
+    dll.reverse(dll.head, dll.tail);
+    dll.display();
+    
+    return 0;
+}
