62a09ed9b74692b7a5ccefe267ed03f35b0db58c
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-21 21:09:49 +0530

    Create deleteAtEndDLL.cpp

---
+++ Doubly Linked  List/deleteAtEndDLL.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    
+    Node(int data){
+        val = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+class DoublyLinkedList{
+public:
+    Node* head;
+    Node* tail;
+
+    DoublyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void display(){
+        if (head == NULL) {
+            cout << "The list is empty." << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }cout<<endl;
+    }
+
+    void insertAtEnd(int val){
+        Node* newNode = new Node(val);
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+        return;
+    }
+
+    void deleteAtEnd(){
+        if(head==NULL) return;
+
+        Node* temp = tail;
+        tail = tail->prev;
+        if(tail==NULL){
+            head=NULL;
+        }else{
+            tail->next = NULL;
+        }
+        free(temp);
+        return;
+    }
+};
+
+int main(){
+    
+    DoublyLinkedList dll;
+    dll.insertAtEnd(1);
+    dll.display();
+    dll.insertAtEnd(2);
+    dll.display();
+    dll.insertAtEnd(3);
+    dll.display();
+
+    dll.deleteAtEnd();
+    dll.display();
+    
+    return 0;
+}
