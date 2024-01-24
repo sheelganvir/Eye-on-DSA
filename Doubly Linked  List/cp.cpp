aee8186db914bb115c681afe772423593578eb58
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-24 22:06:52 +0530

    update DLL

--- Doubly Linked  List/cp.cpp
+++ Doubly Linked  List/cp.cpp
@@ -1,40 +1,94 @@
+/*
+    *********** TRAVERSING IN REVERSE ORDER ***********
+
+    Given the head of a singly linked list, print the linked list in reverse order
+*/
+
+
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<int> plusOne(vector<int>& v) {
-    int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
-                v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
-            }
-        }
-    return v;
+class Node{
+public:
+    int val;
+    Node* next;
 
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList{
+public:
+    Node* head;
+    LinkedList(){
+        head=NULL;
+    }
+
+    void insertAtTail(int val){
+        Node* new_node = new Node(val);
+        if(head==NULL){
+            head=new_node;
+            return;
+        }
+
+        Node *temp = head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display(){ 
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << "->";
+            temp = temp->next;
+        } 
+        cout << "NULL"<<endl;
+    }
+};
+
+Node* partition(Node* head, int x) {
+    Node *left = new Node(0);
+    Node *right = new Node(0);
+    
+    Node *leftTail = left;
+    Node *rightTail = right;
+    
+    while(head != NULL){
+        if(head->val < x){
+            leftTail->next = head;
+            leftTail = leftTail->next;
+        }
+        else{
+            rightTail->next = head;
+            rightTail = rightTail->next;
+        }
+        head = head->next;
+    }
+    
+    leftTail->next = right->next;
+    rightTail->next = NULL;
+    
+    return left->next;
 }
 
 int main(){
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(9);
-    v.push_back(9);
-    int n=v.size();
-    for(int i =0 ; i<n ; i++){
-        cout<<v[i]<<" ";
-    }cout<<endl;
+    
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(8);
+    ll.insertAtTail(4);
+    ll.insertAtTail(7);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.display();
 
-    plusOne(v);
-    for(int i =0 ; i<n ; i++){
-        cout<<v[i]<<" ";
-    }cout<<endl;
+    partition(ll.head,7);
+        ll.display();
     return 0;
 }
