312612b44e149774e8fad0ab39229a5749366f42
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-25 22:18:46 +0530

    update CLL

--- Circular Linked List/insertAtEndCLL.cpp
+++ Circular Linked List/insertAtEndCLL.cpp
@@ -30,35 +30,35 @@ class CircularLinkedList{
         } while (temp!=head);
         cout<<endl;
     }
-    void insertAtStart(int val){
-        Node* new_node = new Node(val);
-        if (head == NULL) {
-            head = new_node;
-            new_node->next = head;   // circular linked list
-            return;
-        }
 
-        Node* tail = head;
-        while(tail->next != head){
-            tail=tail->next;
-        }
-        // now tail is pointing to the last node
-
-        tail->next = new_node;
-        new_node->next = head;
+    void insertAtEnd(int val){
+       Node * new_node = new Node(val);
+       if(head==NULL){
         head = new_node;
+        new_node->next = head;
+        return;
+       }
+
+       Node* tail = head;
+       while(tail->next!=NULL){
+        tail=tail->next;
+       }
+       // here tail is pointing to the last node
+
+       tail->next = new_node;
+       new_node->next = head;
     }
 };
 
 int main(){
     
     CircularLinkedList cll;
-    cll.insertAtStart(1);
-    cll.insertAtStart(2);
-    cll.insertAtStart(3);
-    cll.insertAtStart(4);
-    cll.insertAtStart(5);
-    cll.insertAtStart(6);
+    cll.insertAtEnd(1);
+    cll.insertAtEnd(2);
+    cll.insertAtEnd(3);
+    cll.insertAtEnd(4);
+    cll.insertAtEnd(5);
+    cll.insertAtEnd(6);
 
     cll.display();
     
