c188543a58b966f68402705bebcd4ab3f4342e3d
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-26 16:44:44 +0530

    update list

--- Lists/delete.cpp
+++ Lists/delete.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 int main(){
-    list<int> l1 = {1,2,3,4};
+    list<int> l1 = {1,2,3,4,5,6};
     
     for(auto itr = l1.begin(); itr!=l1.end() ; itr++){
         cout<<*itr<<" ";
     }cout<<endl;
 
-    //reverse traversal
-    for(auto itr = l1.rbegin(); itr!=l1.rend() ; itr++){
+    auto s_itr = l1.begin();
+    advance(s_itr,2);   //s_itr pointing to the 3rd element
+
+    auto e_itr = l1.begin();
+    advance(e_itr,4);   // e_itr pointing to the 5th element
+
+    l1.erase(s_itr,e_itr);
+    for(auto itr = l1.begin(); itr!=l1.end() ; itr++){
         cout<<*itr<<" ";
     }cout<<endl;
+
     return 0; 
 }
