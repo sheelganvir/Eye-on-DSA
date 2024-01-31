b38e3420fe797adcbf514a303ac6ac4b154ddc22
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-01-31 22:06:49 +0530

    Create previousGreaterElement.cpp

---
+++ Stacks/previousGreaterElement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+vector<int> nge(vector<int> &arr){
+    reverse(arr.begin(),arr.end());
+    int n = arr.size();
+    vector<int>output(n,-1);
+    stack<int>st;
+    st.push(0);
+    for(int i=1 ; i<n ; i++){
+        while(!st.empty() and arr[i]>arr[st.top()]){
+            output[st.top()]=arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(not st.empty()){  // not mandatory as we had already initialize the output array with -1
+        output[st.top()] = -1;
+        st.pop();
+    }
+    reverse(output.begin(),output.end());
+    return output;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v;
+    while(n--){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    vector<int>result = nge(v);
+    for(int i=0 ; i<result.size() ; i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
