bc9bd7c4f03f2e033e005e4e706a0eb0122a7900
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-02-06 23:20:57 +0530

    Update getMin.cpp

--- Stacks/getMin.cpp
+++ Stacks/getMin.cpp
@@ -12,3 +12,58 @@
 
 */
 
+#define ll long long int 
+class MinStack {
+public:
+    stack<ll>st;
+    ll min;
+    MinStack() {
+        this->min = INT_MAX;
+    }
+    
+    void push(int val) {
+        if(this->st.empty()){
+            this->min = val;
+            this->st.push(val);
+        }else{
+            this->st.push(val-this->min);
+            if(this->min > val){
+                this->min = val;
+            }
+        }
+    }
+    
+    void pop() {
+        if(not this->st.empty()){
+            if(this->st.top()>=0){
+                this->st.pop();
+            }else{
+                this->min = this->min - this->st.top();
+                this->st.pop();
+            }
+        }
+    }
+    
+    int top() {
+        if(this->st.size() == 1){
+            return this->st.top();
+        }else if(this->st.top() < 0){
+            return this->min;
+        }else{
+            return this->min + this->st.top();
+        }
+    }
+    
+    int getMin() {
+        return this->min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
