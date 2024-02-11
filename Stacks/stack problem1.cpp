041093a139d06c74c4d77ea87c64114f0d1d06f5
sheel-backup <155340880+sheel-backup@users.noreply.github.com>
2024-02-11 10:16:34 +0530

    Create stack problem1.cpp

---
+++ Stacks/stack problem1.cpp
@@ -0,0 +1,41 @@
+/*
+    Copy contents of one stack to another in same order
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> copyStack(stack<int> &input){
+    stack<int>temp;
+    while(not input.empty()){
+        // do the process till the time input stack doesn't becaome empty
+        int curr = input.top();
+        input.pop();
+        temp.push(curr);
+    }
+
+    stack<int>result;
+    while(not temp.empty()){
+        int curr = temp.top();
+        temp.pop();
+        result.push(curr);
+    }
+
+    return result;
+}
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    stack<int>res = copyStack(st);
+    while(not res.empty()){
+        int curr = res.top();
+        res.pop();
+        cout<<curr<<endl;
+    }
+    return 0;
+}
