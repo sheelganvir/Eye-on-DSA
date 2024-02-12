e6182a5e35874fc094fb4787829180c8ccbb930a
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-02-12 20:57:39 +0530

    Update copy_Stack.cpp

--- Stacks/copy_Stack.cpp
+++ Stacks/copy_Stack.cpp
@@ -31,8 +31,9 @@ int main(){
     st.push(2);
     st.push(3);
     st.push(4);
-    
+
     stack<int>res = copyStack(st);
+    
     while(not res.empty()){
         int curr = res.top();
         res.pop();
