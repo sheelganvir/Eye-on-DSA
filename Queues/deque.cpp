9831b1ee13682e99f57f71236acfff2a56864819
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-08 09:20:40 +0530

    Update deque.cpp

--- Queues/deque.cpp
+++ Queues/deque.cpp
@@ -5,9 +5,19 @@
 */
 
 #include <iostream>
+#include <deque>
 using namespace std;
 
 int main(){
-    
+    deque<int> dq;
+    dq.push_back(10);
+    dq.push_back(20);
+    dq.push_back(30);
+    dq.push_front(0);
+    dq.push_front(50);
+    while(not dq.empty()){
+        cout<<dq.front()<<" ";
+        dq.pop_front();
+    }
     return 0;
 }
