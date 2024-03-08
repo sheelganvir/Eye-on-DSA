550823ebb3968d6a31399fb09df298097876b99f
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-08 09:46:03 +0530

    Create sliding_window.cpp

---
+++ Queues/sliding_window.cpp
@@ -0,0 +1,16 @@
+/*
+    You are given an array of integers nums, there is a sliding window of size k 
+    which is moving from very left of the array to the vey right. You can only
+    see the k numbers in the window. Each time the sliding window moves rightwards
+    by one position. Return the max sliding windows which is basically contains the
+    max element in each window.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+} 
