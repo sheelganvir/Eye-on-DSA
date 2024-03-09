727175e9c9b591d314948f973c12ffd193b7fc2f
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-10 00:02:54 +0530

    Update countPossibleMarks.cpp

--- Sets/countPossibleMarks.cpp
+++ Sets/countPossibleMarks.cpp
@@ -1,3 +1,15 @@
+/*
+    Given the number of questions as n, and the marks for the correct answer as p and q marks
+    for the incorrect answer. One can either attempt to solve the question in an examination
+    and get either p maarks if the answer is right or q marks if the answer is wrong, or leave 
+    the question unattempted and get 0 marks. The task is to find the count of all the different 
+    possible marks that one can score in the examination.
+
+    Input: n=2 , p=1 , q=-1
+    Output: 5
+    Explanation : The different possible marks are : -2, -1, 0, 1,  2
+*/
+
 #include <iostream>
 #include <unordered_set>
 using namespace std;
