85d675f06e34dbb13e6dd02c40d23cb703b6ccfc
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-11 19:53:20 +0530

    Create checkStringEqual.cpp

---
+++ Hashmap/checkStringEqual.cpp
@@ -0,0 +1,24 @@
+/*
+    Can you make the strings equal?
+
+    Given an array of strings. You can move any number of characters from one string to any other string 
+    any number of times. You hust have to make them all equal.
+    Print "Yes" if you can make every string in the array equal by using any number of operations
+    otherwise print "No".
+
+    Input: ["collegeee", "coll", "collegge"]
+    Output: Yes
+    Explanation: string at 1 index can take two 'e' from 0 index string and one 'g' from 2 index string.
+
+    Input: ["wall", "ah", "wallahah"]
+    Output: No
+    Explanation: Here we don't have enough number of characters to make all strings equal 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
