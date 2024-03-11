9486c320141175c44a454e41a53126c54b8c84a7
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-11 23:07:38 +0530

    Create checkAnagram.cpp

---
+++ Hashmap/checkAnagram.cpp
@@ -0,0 +1,25 @@
+/*
+    Check whether tw strings are anagram of each other. Return true if they are else reu=turn false.
+    An anagram of a string is another string that contains the same characters, only the order of
+    characters can be different. For example, "abcd" and "dabc" are an anagram of each other.
+
+    Input1 : triangle
+             integral
+    Output1: True
+
+    Input2 : anagram
+             grams
+    Output2: False
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
