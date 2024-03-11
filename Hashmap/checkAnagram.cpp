1bbff73b94b6488f0481eca5197e0e8a873b850a
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-11 23:30:08 +0530

    Update checkAnagram.cpp

--- Hashmap/checkAnagram.cpp
+++ Hashmap/checkAnagram.cpp
@@ -19,7 +19,37 @@
 #include <unordered_map>
 using namespace std;
 
+bool checkAnagram(string s1, string s2){
+    //if size are not equal
+    if (s1.size() != s2.size()) return false;
+
+    unordered_map<char,int> m;
+
+    for(auto c1:s1){
+        m[c1]++;    //storing characters and their frequency in s1
+    }
+
+    for(auto c2:s2) {
+        if(m.find(c2)==m.end()){
+            return false;
+        }else{
+            m[c2]--;
+        }
+    }
+
+    for(auto ele:m){
+        if(ele.second!=0){
+            return false;
+        } 
+    }
+    return true;
+}
+
 int main(){
     
+    string s1, s2;
+    cin>>s1>>s2;
+
+    cout<<(checkAnagram(s1,s2)? "True":"False")<<endl;
     return 0;
 }
