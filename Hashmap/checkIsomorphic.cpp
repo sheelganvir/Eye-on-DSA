04c82bdb2d79d6f8cdb0390489f33a68b1e8ce87
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-12 17:46:14 +0530

    Update checkIsomorphic.cpp

--- Hashmap/checkIsomorphic.cpp
+++ Hashmap/checkIsomorphic.cpp
@@ -1,15 +1,19 @@
 /*
-    Check whether tw strings are anagram of each other. Return true if they are else reu=turn false.
-    An anagram of a string is another string that contains the same characters, only the order of
-    characters can be different. For example, "abcd" and "dabc" are an anagram of each other.
+    Check whether two strings are isomorphic of each other. Return true if they are else return false.
 
-    Input1 : triangle
-             integral
+    Isomorphic: Two strings are isomorphic of each other if there is a one to one mapping is possible for 
+                every charcters of the first string to every character of second string and all occurreneces
+                of every character in first character in first string maps to the same character in the second string.
+
+    Input1: aab
+            xxy
+
     Output1: True
 
-    Input2 : anagram
-             grams
-    Output2: False
+    Input2: abcdec
+            viouog
+
+    Output2: False 
 
     ********************************************************************************
             Time complexity: O(N), N is sum of lengths of s1 and s2
@@ -26,30 +30,32 @@
 #include <unordered_map>
 using namespace std;
 
-bool checkAnagram(string s1, string s2){
+bool checkNoOneToManyMapping(string s1, string s2){
+
+    unordered_map<char,char> m;
+    for(int i=0 ; i<s1.size() ; i++){
+        if(m.find(s1[i])!=m.end()){
+            if(m[s1[i]]!=s2[i]){
+                return false;
+            }
+        }else{
+            m[s1[i]] = s2[i];
+        }
+    }
+    return true;
+}
+
+bool checkIsomorphic(string s1, string s2){
     //if size are not equal
     if (s1.size() != s2.size()) return false;
 
-    unordered_map<char,int> m;
+    //checking one to many mapping from s1->s2
+    bool s1s2 = checkNoOneToManyMapping(s1,s2);
 
-    for(auto c1:s1){
-        m[c1]++;    //storing characters and their frequency in s1
-    }
+    //checking one to many mapping from s2->s1
+    bool s2s1 = checkNoOneToManyMapping(s2,s1);
 
-    for(auto c2:s2) {
-        if(m.find(c2)==m.end()){
-            return false;
-        }else{
-            m[c2]--;
-        }
-    }
-
-    for(auto ele:m){
-        if(ele.second!=0){
-            return false;
-        } 
-    }
-    return true;
+    return s1s2 and s2s1;
 }
 
 int main(){
@@ -57,6 +63,6 @@ int main(){
     string s1, s2;
     cin>>s1>>s2;
 
-    cout<<(checkAnagram(s1,s2)? "True":"False")<<endl;
+    cout<<(checkIsomorphic(s1,s2)? "True":"False")<<endl;
     return 0;
 }
