613c44a46957104dc83aa2340e01eac7554d2cfa
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-13 20:58:33 +0530

    Update permutationsStrings.cpp

--- Backtracking/permutationsStrings.cpp
+++ Backtracking/permutationsStrings.cpp
@@ -1,7 +1,28 @@
+/*
+    Write a program to print all permutations of the given string s
+    in lexicographically sorted order.
+
+    Input: PQR
+    Output: PQR PRQ QPR QRP RPQ RQP
+*/
+
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+void permutation(string &str, int i){
+    //base case
+    if(i == str.size()){
+        cout<<str<<"\n";
+        return;
+    }
+    for(int j = i ; j<str.size() ; j++){
+        swap(str[i],str[j]);
+        permutation(str, i+1);
+    }
+}
+
+int main(){ 
+    string str = "abc";
+    permutation(str,0);
     return 0;
 }
