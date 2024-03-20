feb75730d234c94f1ac54a4ef0510e016d895806
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-03-20 07:33:58 +0530

    Create class.cpp

---
+++ Binary trees/class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int v){
+        value = v;
+        left = right = NULL;
+    }
+};
+
+int main(){
+
+    Node* root  = new Node(80);    
+    return 0;
+}
