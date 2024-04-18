75572a548c5474b5051dbac227a9a49c266c27fb
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-04-18 21:57:29 +0530

    Update postorderTraversal.cpp

--- Binary trees/postorderTraversal.cpp
+++ Binary trees/postorderTraversal.cpp
@@ -25,6 +25,8 @@ void postorderTraversal(Node* rootNode){
     cout << rootNode->value << " ";
     postorderTraversal(rootNode->left);
 }
+
+
 int main(){
     Node* root  = new Node(2);    
     root->left = new Node(4);
