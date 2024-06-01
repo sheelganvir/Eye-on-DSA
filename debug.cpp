507b9b24ec74074caa8f996e4e25b7aa6b2a3705
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-06-01 19:24:43 +0530

    update recent files

--- debug.cpp
+++ debug.cpp
@@ -1,27 +1,52 @@
+/*
+    An encoded string(s) is given, and the task is to decode it. The encoding pattern is that 
+    the occurence of the string is given at the starting of the string and each string is 
+    enclosed by square brackets
+    Note: The occurence of a sngle string is less than 1000
+
+    Input1: s = 1[b]
+    Output1: b
+
+    Input2: s = 3[b2[ca]]
+    Output: bcacabcacabcaca
+
+    Time: O(n), where n is the length of decoded string
+    Space: O(n), where n is the length of decoded string
+*/
+
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
+#include <stack>
+
 using namespace std;
 
-void rotate(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    
-    for(int i=0 ; i<n ; i++){
-        for(int j=0 ; j<i ; j++){
-            int n1 = matrix[i][j];
-            int n2 = matrix[j][i];
-            swap(matrix[i][j],matrix[j][i]);
+int maxDepth(string s) {
+    int count=0;
+    int max=INT16_MIN;
+    stack<char>st;
+    for(auto i:s)
+    {
+        if(i=='(')
+        {
+            st.push(i);
+            count++;
         }
+        if(i==')'){
+            st.pop();
+            count=st.size();
+        }
+        if(count>max)
+        max=count; 
     }
-    
-    for(int i=0 ; i<n ; i++){
-        reverse(matrix[i].begin(),matrix[i].end());
-    }
+    return max;
 }
 
 int main(){
-    vector<vector<int>> nums = {{1,2,3},{4,5,6},{7,8,9}};
-    rotate(nums);
     
+    string str = "(1+(2*3)+((8)/4))+1";
+
+    cout<<maxDepth(str);
     return 0;
 }
